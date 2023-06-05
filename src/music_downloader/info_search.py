f0cf55aededdd95769fc1ef6825a9a38f8e56ece
teemo19 <cmaldonado0@ucol.mx>
2023-06-05 13:44:41 -0600

    adding line: search using videosearch

--- src/music_downloader/info_search.py
+++ src/music_downloader/info_search.py
@@ -2,9 +2,13 @@
 # Date: 05/06/2023
 # Linkedin: https://www.linkedin.com/in/alejandro-maldonado-a9a908148/
 from abc import ABC
+from youtubesearchpython import VideosSearch
+
+ZERO = 0
 
 class info_search(ABC):
     @staticmethod
     def search_link():
-        pass
+        search = VideosSearch(["result"][ZERO].get("title"))
+        
     
