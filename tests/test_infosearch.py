8106929671c96c96bd4b34f13247cde86c967396
teemo19 <cmaldonado0@ucol.mx>
2023-06-06 06:37:48 -0600

    version 0 - en construccion

--- tests/test_infosearch.py
+++ tests/test_infosearch.py
@@ -1,16 +1,14 @@
 import unittest
-from src.music_downloader.info_search import info_search
+from code import info_search
 
-class test_infosearch(unittest.TestCase):
-    def test_search(self):
-        desc1 = "la chispa adecuada"
-        desc2 = "pxndx los malaventurados"
-        data1 = info_search.song_info(desc1)
-        data2 = info_search.song_info(desc2)
-        result1 = info_search.get_link(data1)
-        result2 = info_search.get_link(data2)
-        self.assertEqual(result1,"https://www.youtube.com/watch?v=jnDjEHyhFpU")
-        self.assertEqual(result2,"https://www.youtube.com/watch?v=CF72RkHcKqs")
-        
-if __name__ == "__main__":
+class TestInfoSearch(unittest.TestCase):
+    def test_song_info(self):
+        # Test code here
+        pass
+
+    def test_get_link(self):
+        # Test code here
+        pass
+
+if __name__ == '__main__':
     unittest.main()
